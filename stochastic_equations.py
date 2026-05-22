cec7c9137668291a86fbf92b4de15bd3e9c4f5f3
arnavydv <aarnavyadvv@gmail.com>
2026-05-23 00:50:48 +0530

    imporved fsde 2

--- stochastic_equations.py
+++ stochastic_equations.py
@@ -1,18 +1,20 @@
 import numpy as np 
-def forward_pass(time:float,N:int,mu:float,rate_in_percent:float,sigma_in_percent:float,pi_star,w_t):
+from neural_networks import pi_star
+model=pi_star()
+def forward_pass(time:float,N:int,mu:float,rate_in_percent:float,sigma_in_percent:float,model,w0):
     rate=rate_in_percent/100
     sigma=sigma_in_percent/100
     dt = time/N
-    time_grid=np.linspace(0,time,N+1)
-    wealth_path=np.zeros(N+1)
-    wealth_path=w_t
+    time_array=np.linspace(0,time,N+1)
+    wealth_array=np.zeros(N+1)
+    wealth_array=w0
     dW = np.random.normal(0, np.sqrt(dt), N)
     for i in range(N):
-        t=time_grid[i]
-        W=wealth_path[i]
-        pi=pi_star(t,W)
+        t=time_array[i]
+        W=wealth_array[i]
+        pi=model(t,W)
     drift= rate*W+np.dot(pi,mu-rate)
     diffusion=(np.dot(pi,sigma))*dW[i]
-    wealth_path[i+1]=W+(drift*dt)+diffusion
-    return time_grid,wealth_path
+    wealth_array[i+1]=W+(drift*dt)+diffusion
+    return time_array,wealth_array
 
