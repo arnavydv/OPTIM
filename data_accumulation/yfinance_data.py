6912d586a0c87aa85f75932a25db67a290a6bfd4
arnavydv <aarnavyadvv@gmail.com>
2026-05-24 23:54:40 +0530

    gathered data

--- data_accumulation/yfinance_data.py
+++ data_accumulation/yfinance_data.py
@@ -2,7 +2,8 @@ import yfinance as yf
 import pandas as pd 
 data=yf.Ticker('AAPL')
 
-aapl_historical = data.history(start="2025-06-02", end="2025-06-07", interval="1h")
+aapl_historical = data.history(start="2023-05-23", end="2026-05-23", interval="1d")
 """so this aapl_historical ticker is a pandas dataframe so 
 we are going to use pandas here"""
-aapl_historical['Close']
+final_data=aapl_historical.to_csv("C:\\Users\\Admin\\Desktop\\FBSDE PROJECT\\data_accumulation\\aapl_data.csv")
+print(type(final_data))
