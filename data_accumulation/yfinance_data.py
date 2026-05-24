ddeeb6c149c363903a854bc6b7d9d1c44ccf8875
arnavydv <aarnavyadvv@gmail.com>
2026-05-24 23:42:19 +0530

    aapl_historical

--- data_accumulation/yfinance_data.py
+++ data_accumulation/yfinance_data.py
@@ -1,5 +1,8 @@
 import yfinance as yf
+import pandas as pd 
 data=yf.Ticker('AAPL')
 
 aapl_historical = data.history(start="2025-06-02", end="2025-06-07", interval="1h")
-print(aapl_historical)
+"""so this aapl_historical ticker is a pandas dataframe so 
+we are going to use pandas here"""
+aapl_historical['Close']
